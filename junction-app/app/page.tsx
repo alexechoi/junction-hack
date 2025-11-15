7fb06b3d3890096e23af3bbb79eca452452f8bda
alexchvod <uceiac8@ucl.ac.uk>
2025-11-15 20:59:36 +0200

    simplify home

--- junction-app/app/page.tsx
+++ junction-app/app/page.tsx
@@ -1,14 +1,12 @@
 import { Demo } from "@/components/landing/Demo";
 import { Hero } from "@/components/landing/Hero";
 import { LandingFooter } from "@/components/landing/Footer";
-import { TrustScore } from "@/components/landing/TrustScore";
 
 export default function Home() {
   return (
     <main className="bg-zinc-950 text-white">
       <Hero />
       <Demo />
-      <TrustScore />
       <LandingFooter />
     </main>
   );
