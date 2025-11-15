77e137d00b7e5250dc3673fd93a536d0a168fe6d
alexchvod <uceiac8@ucl.ac.uk>
2025-11-15 11:59:14 +0200

    remove home from logged out nav

--- junction-app/components/Nav.tsx
+++ junction-app/components/Nav.tsx
@@ -77,9 +77,6 @@ export default function Nav() {
               </>
             ) : (
               <>
-                <Link href="/" className={`${linkTheme} rounded-md px-3 py-2`}>
-                  Home
-                </Link>
                 <Link
                   href="/auth"
                   className={`${buttonTheme} rounded-full px-4 py-2 text-sm font-medium transition`}
